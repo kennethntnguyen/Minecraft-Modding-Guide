e4c65ff33d36473a597d5838a2edbaae2545ca8d
Kenneth Nguyen <kennethntnguyen@gmail.com>
2020-05-12 23:00:41 -0700

    removed python script used to make markdown commands using image filenames

--- screenshots/make_png_markdown_commands.py
+++
@@ -1,6 +0,0 @@
-import os
-f = open("images_markdown.txt", "w+")
-for file in os.listdir():
-    if file.lower().endswith(".png"):
-      f.write('![' + file + ']' + '(/screenshots/' + file + ')\n')
-f.close()
